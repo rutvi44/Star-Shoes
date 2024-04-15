28bd40a17358a7edd106ad323c0bffde06b638a6
Rutvi Mistry <rmistry9104@conestogac.on.ca>
2024-04-15 17:12:21 -0400

    cart(not completed)

--- app/src/main/java/com/example/group6_prog3210_finalproject/Cart.java
+++ app/src/main/java/com/example/group6_prog3210_finalproject/Cart.java
@@ -3,6 +3,7 @@ package com.example.group6_prog3210_finalproject;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Cart extends AppCompatActivity {
 
