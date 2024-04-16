3a53e5a2592eb815b150c2709b6a05c15bd41be4
Rutvi Mistry <rmistry9104@conestogac.on.ca>
2024-04-15 22:21:19 -0400

    cart(not completed) and About page done

--- app/src/main/java/com/example/group6_prog3210_finalproject/ActivityAbout.java
+++ app/src/main/java/com/example/group6_prog3210_finalproject/ActivityAbout.java
@@ -1,9 +1,11 @@
 package com.example.group6_prog3210_finalproject;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,18 @@ public class ActivityAbout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        // Find the ImageView button by its ID
+        ImageView btnBack = findViewById(R.id.btnBack);
+
+        // Set an OnClickListener for the button
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the desired activity
+                Intent intent = new Intent(ActivityAbout.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
